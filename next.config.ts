57be09cf1869d6ccd1b123b4e61cf39bea0bb1d4
juontajasimo-hub <juontajasimo@gmail.com>
2026-03-23 12:03:53 +0200

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/raide-next',
+  assetPrefix: '/raide-next',
   images: {
     unoptimized: true,
   },
